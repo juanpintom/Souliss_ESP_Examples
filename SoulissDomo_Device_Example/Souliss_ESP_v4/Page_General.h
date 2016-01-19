4407396d1886d39848eadb58ce4643a67122c8c8
Juan Pinto <juanpintom@gmail.com>
2016-01-19 20:27:38 +0100

    Minor Changes and code Cleaning

--- SoulissDomo_Device_Example/Souliss_ESP_v4/Page_General.h
+++ SoulissDomo_Device_Example/Souliss_ESP_v4/Page_General.h
@@ -14,13 +14,12 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
 
 <form action="" method="post">
 <table border="0"  cellspacing="0" cellpadding="3" >
-  <tr>
-    <td align="left" style="width:190px">Node Name</td>
+  <tr><td align="left" style="width:190px">Node Name</td>
     <td align="right"><input type="text" id="devicename" name="devicename" value="" maxlength="10"></td>
-  </tr>
-  </table>
-  <td><hr></td>
+    </tr>
+    </table>
   
+  <hr>
   <div align="center" style="width:380px"><strong>Lights Configuration:(L1-L2-L3)</strong></div>
   
   <table style="width:500px">
@@ -42,7 +41,6 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
       <option value="2">PWM MODE</option>
       <option value="3">RGB MODE (GREEN)</option>
       <option value="4">PIR MODE</option>
-      <option value="5">PIR ALARM MODE</option>
       <option value="6">PULSE MODE</option>
     </select>
     </td></tr>
@@ -53,29 +51,15 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
       <option value="2">PWM MODE</option>
       <option value="3">RGB MODE (BLUE)</option>
       <option value="4">PIR MODE</option>
-      <option value="5">PIR ALARM MODE</option>
       <option value="6">PULSE MODE</option>
     </select>
     </td></tr>
-
   </table>
   
-  <td><hr></td>  
+  <hr>  
   <div align="center" style="width:380px"><strong>Sensors Configuration:(S1-S2-S3)</strong></div>
-  <table>
-  <tr><td id="ComboA">Sensors Configuration:</td><td>
-  <select  id="byte0" name="byte0" onchange="getComboA(this)">
-    <option value="0">None</option>
-    <option value="1">DHT |........|.......</option>
-    <option value="2">.......| LDR |.......</option>
-    <option value="3">.......|........| DALLAS</option>
-    <option value="4">DHT | LDR |.......</option>
-    <option value="5">DHT |........| DALLAS</option>
-    <option value="6">.......| LDR | DALLAS</option>
-    <option value="7">DHT | LDR | DALLAS</option>
-  </select>
-  </td></tr>
   
+  <table>
   <tr><td>(S1) GPIO14 Configuration: </td><td>
     <select  id="S1" name="S1" onchange="getComboS1(this)" style="width:180px">
       <option value="0">None</option>
@@ -84,7 +68,6 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
     </select>
     <td align="left" id="qty">Qty: <input type="text" id="dallas_qty" name="dallas_qty" size="2" value="1"></td>
     </td></tr>
-  <tr>
     
   <tr><td>(S2) GPIO16 Configuration: </td><td>
     <select  id="S2" name="S2" onchange="getComboS2(this)"style="width:180px">
@@ -96,8 +79,7 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
      <select  id="dht_type" name="dht_type">
         <option value="0">DHT11</option>
         <option value="1">DHT22</option>
-        </select>
-    </td>
+        </select></td>
     </td></tr>
   
   <tr><td>(S3) Analog Configuration: </td><td>
@@ -111,8 +93,8 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
   <td id="API_text"><input type="text" id="API" name="API" value="" maxlength="32"> API Key</td></tr>
   </table>
   
-  <td><hr></td>  
-  
+  <hr>
+  <table>
   <tr><td>(S4) Configuration: </td><td>
     <select  id="S4" name="S4" onchange="getComboS4(this)"style="width:180px">
       <option value="0">None</option>
@@ -124,7 +106,13 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
       <option value="6">BUTTONS_2_STATE</option>
     </select>
     </td></tr>
-  <td><hr></td> 
+  <tr id="Thresold">
+    <td align="left" id="cap_thresold_line"> Capacitive Thresold:</td>
+    <td><input type="text" id="cap_thresold" name="cap_thresold" size="2" value="5"></td></tr>    
+    </table>
+  
+  <hr>
+  <table>
   <tr><td>(S5) Configuration: </td><td>
     <select  id="S5" name="S5" onchange="getComboS5(this)"style="width:180px">
       <option value="0">None</option>
@@ -136,56 +124,27 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
       <option value="6">220V_OPTO_AND_RELAY</option>
     </select>
     </td></tr>
-  
-  <div align="center" style="width:280px"><strong>Other Sensors: (S4-S5)</strong></div>
-  <table style="width:500px">
-  <tr><td style="width:170px">Others: (S4-S5) </td><td>
-    <select id="byte2" name="byte2" onchange="getComboC(this)">
-      <option value="0">None</option>
-      <option value="1">(S5) CAPACITIVE</option>
-      <option value="2">(S4) RELAY</option>
-      <option value="3">(S5) PRESSURE SENSOR</option>
-      <option value="4">(S5) CAPACITIVE /w DEBUG</option>
-      <option value="5">(S5) BUTTONS</option>
-      <option value="6">(S5) BUTTONS_PULLUP</option>
-      <option value="7">(S5) ALARM_ENDSTOP</option>
-      <option value="8">(S5) BUTTONS_2_STATE</option>
-      <option value="9">(S4) PULSE_OUTPUT</option>
-      <option value="10">(S4) GARAGE_DOOR</option>
-      <option value="11">(S4) WINDOW_CURTAIN</option>
-      <option value="12">(S4) 220V_OPTO_AND_RELAY</option>
-    </select>
-    </td></tr>
-  
- <tr id="Thresold">
-  <td align="left" id="cap_thresold_line"> Capacitive Thresold:</td>
-  <td><input type="text" id="cap_thresold" name="cap_thresold" size="2" value="5"></td>
- </tr>
- <tr id="Altitude">
-  <td align="left" id="Altitude_line"> Altitude:</td>
-  <td><input type="text" id="Altitude_id" name="Altitude_id" size="2" value="20"></td>
- </tr>
- </table>
+  <tr id="Altitude">
+    <td align="left" id="Altitude_line"> Altitude:</td>
+    <td><input type="text" id="Altitude_id" name="Altitude_id" size="2" value="20"></td></tr>
+    </table>
 
-  <td><hr></td>    
-    
+  <hr>    
   <table>
-    <tr>
-    <td align="left" style="width:230px">Receive IR on this Node? (S6):</td>
+    <tr><td align="left" style="width:230px">Receive IR on this Node? (S6):</td>
     <td><input type="checkbox" id="IR_ENABLE" name="IR_ENABLE" onclick="getChk(this)"></td>
     </tr></table>
     
-  <td style="width:500px"><hr></td>
+  <hr>
   <table>
-    <tr>
-    <td align="left" style="width:230px"> Node is USART Bridge?: (S4)</td>
+    <tr><td align="left" style="width:230px"> Node is USART Bridge?: (S4)</td>
     <td><input type="checkbox" id="usartbridge" name="usartbridge" onclick="getUSART(this)"></td>
     </tr></table>
   
-  <td style="width:500px"><hr></td>
+  <hr>
   <table>
     <tr><td align="left" style="width:230px"><strong>DEBUG Configuration:</strong></td>
-    <td><input type="checkbox" id="DEBUG" name="DEBUG" onclick="getDebug(this)"> <-Check to Show</td></tr></table>
+    <td><input type="checkbox" id="DEBUG" name="DEBUG" onclick="getDebug(this)">Check to Show</td></tr></table>
     
   <table id="LOG">
     <tr><td><input type="checkbox" id="ALL_LOG" name="ALL_LOG" onclick="getAllLog(this)"><strong>Enable All </strong></td></tr>
@@ -201,10 +160,12 @@ const char PAGE_AdminGeneralSettings[] PROGMEM =  R"=====(
     <tr><td><input type="checkbox" id="DEBUG_IR_FULL" name="DEBUG_IR_FULL">Debug IR_FULL</td></tr>
     <tr><td><input type="checkbox" id="DEBUG_PLC" name="DEBUG_PLC">Debug PLC</td></tr>
   </table>
-    <td style="width:500px"><hr></td>
+  
+  <hr>
   <table>  
   <tr><td colspan="2" align="center"><input type="submit" style="width:150px" class="btn btn--m btn--blue" value="Save"></td></tr>
   </table>
+  
   <strong>Remember Get Souliss Nodes again from the app after Save Settings</strong>
 </form>
 
@@ -231,9 +192,6 @@ function load(e,t,n){
   }
 }
 function getConfig(){
-    document.getElementById("ComboA").style.display = "none";  //QUITAR!!
-    document.getElementById("byte0").style.display = "none";  //QUITAR!!
-    
     if(document.getElementById("S2").value == 1){
       document.getElementById("type").style.display = "block";  //none;  
     }else{
@@ -251,13 +209,14 @@ function getConfig(){
       document.getElementById("API_text").style.display = "none"; 
     } 
     
-    value = document.getElementById("byte2");
-    if(value == 1 || value == 4) {
+    value = document.getElementById("S4");
+    if(value == 1 || value == 2) {
        document.getElementById("Thresold").style.display = "block";  //none;  
     } else { 
        document.getElementById("Thresold").style.display = "none"; 
     }
    
+    value = document.getElementById("S5");
     if(value == 3) {
       document.getElementById("Altitude").style.display = "block";  //none;  
     } else {
@@ -349,27 +308,29 @@ function getComboL3(sel) {
 }
   
   
-function getComboC(sel) {
+function getComboS4(sel) {
   var value = sel.value;
-  if(value == 1 || value == 4) {
+  if(value == 1 || value == 2) {
        document.getElementById("Thresold").style.display = "block";  //none;  
   } else { 
        document.getElementById("Thresold").style.display = "none"; 
   }
-   
+  if(value > 0){
+    document.getElementById("usartbridge").checked = false; 
+  }
+}
+
+function getComboS5(sel) {
+  var value = sel.value;  
   if(value == 3) {
       document.getElementById("Altitude").style.display = "block";  //none;  
   } else {
       document.getElementById("Altitude").style.display = "none";  
   }
-  if(value == 2 || value == 9 || value == 10 || value == 11 || value == 12){
-    document.getElementById("usartbridge").checked = false;  
-  
-  }
 }
 function getUSART(sel){
   if(sel.checked){
-    document.getElementById("byte2").value = 0;   
+    document.getElementById("S4").value = 0;   
   }
 }
   
